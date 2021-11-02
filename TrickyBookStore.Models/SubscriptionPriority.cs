5ca29e702db18dbabaa37d585ee3edd818a0ae00
Nghiep Tran Dai <nghiep.tran.dai@preciofishbone.se>
2021-11-02 14:03:42 +0700

    Initial commit

---
+++ TrickyBookStore.Models/SubscriptionPriority.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrickyBookStore.Models
+{
+    public enum SubscriptionPriority
+    {
+        Free = 0,
+        Paid = 1,
+        CategoryAddicted = 2,
+        Premium = 3
+    }
+}
---
+++ TrickyBookStore.Services/Store/Subscriptions.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using TrickyBookStore.Models;
+
+namespace TrickyBookStore.Services.Store
+{
+    public static class Subscriptions
+    {
+        public static readonly IEnumerable<Subscription> Data = new List<Subscription>
+        {
+            new Subscription { Id = 1, SubscriptionType = SubscriptionTypes.Paid, Priority = (int)SubscriptionPriority.Paid,
+                PriceDetails = new Dictionary<string, double>
+                {
+                   
+                }
+            },
+            new Subscription { Id = 2, SubscriptionType = SubscriptionTypes.Free, Priority = (int)SubscriptionPriority.Free,
+                PriceDetails = new Dictionary<string, double>
+                {
+
+                }
+            },
+            new Subscription { Id = 3, SubscriptionType = SubscriptionTypes.Premium, Priority = (int)SubscriptionPriority.Premium,
+                PriceDetails = new Dictionary<string, double>
+                {
+                   
+                }
+            },
+            new Subscription { Id = 4, SubscriptionType = SubscriptionTypes.CategoryAddicted, Priority = (int)SubscriptionPriority.CategoryAddicted,
+                PriceDetails = new Dictionary<string, double>
+                {
+                  
+                }              
+            },
+            new Subscription { Id = 5, SubscriptionType = SubscriptionTypes.CategoryAddicted, Priority = (int)SubscriptionPriority.CategoryAddicted,
+                PriceDetails = new Dictionary<string, double>
+                {
+                    { "FixPrice", 75 },
+                    // ...
+                },
+                BookCategoryId = 1
+            },
+            new Subscription { Id = 6, SubscriptionType = SubscriptionTypes.CategoryAddicted, Priority = (int)SubscriptionPriority.CategoryAddicted,
+                PriceDetails = new Dictionary<string, double>
+                {
+                    { "FixPrice", 75 },
+                    // ....
+                },
+                BookCategoryId = 3
+            }
+        };
+    }
+}
